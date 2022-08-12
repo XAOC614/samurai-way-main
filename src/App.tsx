b2b338b6790675c8b8560319019286080d9313cc
Сергей Фомин <xaoc615@gmail.com>
2022-08-12 19:25:07 +0300

    add components (les 1-9)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
-
+import Technologies from "./Technologies";
+import Header from "./Header";
 function App() {
   return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
+      <div>
+        <Header/>
+        <Technologies/>
+      </div>
   );
 }
+
+
 
 export default App;
