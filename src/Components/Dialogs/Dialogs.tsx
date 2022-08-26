25d2cd7b6dd25da3a9162d318c1610f2054c1aaf
Сергей Фомин <xaoc615@gmail.com>
2022-08-26 18:26:07 +0300

    Dialogs verstka Commit 21

--- src/Components/Dialogs/Dialogs.tsx
+++ src/Components/Dialogs/Dialogs.tsx
@@ -1,9 +1,36 @@
 import React from "react";
 import c from './Dialogs.module.css'
+
 export const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={c.dialogs}>
+            <div className={c.dialogsItems}>
+                <div className={`${c.dialog} ${c.active}`}>
+                    Dima
+                </div>
+                <div className={c.dialog}>
+                    Andrey
+                </div>
+                <div className={c.dialog}>
+                    Sergey
+                </div>
+                <div className={c.dialog}>
+                    Anna
+                </div>
+                <div className={c.dialog}>
+                    Sasha
+                </div>
+                <div className={c.dialog}>
+                    Valeriy
+                </div>
+            </div>
+            <div className={c.messages}>
+                <div className={c.message}>Hi</div>
+                <div className={c.message}>How are you?</div>
+                <div className={c.message}>You are Best!</div>
+
+
+            </div>
         </div>
     )
 }
