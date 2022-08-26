22259175a2b3a64a8b79f734db91c3c376beb394
Сергей Фомин <xaoc615@gmail.com>
2022-08-26 18:43:09 +0300

    Dialogs NavLink Commit 222

--- src/Components/Dialogs/Dialogs.tsx
+++ src/Components/Dialogs/Dialogs.tsx
@@ -1,27 +1,28 @@
 import React from "react";
 import c from './Dialogs.module.css'
+import {NavLink} from "react-router-dom";
 
 export const Dialogs = () => {
     return (
         <div className={c.dialogs}>
             <div className={c.dialogsItems}>
                 <div className={`${c.dialog} ${c.active}`}>
-                    Dima
+                    <NavLink to='/dialogs/1'>Dima</NavLink>
                 </div>
                 <div className={c.dialog}>
-                    Andrey
+                    <NavLink to='/dialogs/2'>Andrey</NavLink>
                 </div>
                 <div className={c.dialog}>
-                    Sergey
+                    <NavLink to='/dialogs/3'>Sergey</NavLink>
                 </div>
                 <div className={c.dialog}>
-                    Anna
+                    <NavLink to='/dialogs/4'>Anna</NavLink>
                 </div>
                 <div className={c.dialog}>
-                    Sasha
+                    <NavLink to='/dialogs/5'>Sasha</NavLink>
                 </div>
                 <div className={c.dialog}>
-                    Valeriy
+                    <NavLink to='/dialogs/6'>Valera</NavLink>
                 </div>
             </div>
             <div className={c.messages}>
